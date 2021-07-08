33dbec26df7c5086d112ea067416803cc797679f
No-Github <D2hwakH7BS5E@protonmail.com>
2021-07-08 15:19:17 +0800

    update 2021年07月 8日 15:19:17

---
+++ Slider/script2.js
@@ -0,0 +1,20 @@
+function onclick1()
+{
+    window.open("https://github.com/ffffffff0x/f8x");
+}
+function onclick2()
+{
+    window.open("https://github.com/ffffffff0x/Digital-Privacy");
+}
+function onclick3()
+{
+    window.open("https://github.com/ffffffff0x/Dork-Admin");
+}
+function onclick4()
+{
+    window.open("https://github.com/ffffffff0x/AboutSecurity");
+}
+function onclick5()
+{
+    window.open("https://github.com/ffffffff0x/BerylEnigma");
+}
